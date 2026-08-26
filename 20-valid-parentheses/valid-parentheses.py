3856340bf1481d69f871e87f74845e33981e3682
Paras Chauhan <thisisparas@outlook.com>
2026-08-26 22:34:06 +0530

    Time: 2 ms (37.08%) | Memory: 19.3 MB (23.44%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for char in s:
+            if char == '(' or char == '{' or char == '[':
+                stack.append(char)
+            else:
+                if char==')' and stack and stack[-1] == '(':
+                    stack.pop()
+                elif char == '}' and stack and stack[-1] == '{':
+                    stack.pop()
+                elif char == ']' and stack and stack[-1] == '[':
+                    stack.pop()
+                else:
+                    return False
+        return not stack
